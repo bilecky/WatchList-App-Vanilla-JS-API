50806d507a4cf01505dee1f1c909e1f268e20e4c
bilecky <bilecky95@gmail.com>
2023-01-29 18:28:36 +0100

    initial commit & full project upload

---
+++ favMovies.js
@@ -0,0 +1,66 @@
+const searchInput = document.querySelector('.search-input')
+const searchBtn = document.querySelector('.search-btn')
+const moviesBox = document.querySelector('.movies')
+const favMovies = document.querySelector('.movies-fav')
+const watchList = document.querySelector('.watchlist')
+const noMoviesParagraph = document.querySelector('.no-fav-movies')
+const API_KEY = '7b891d'
+
+const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}&t=`
+
+const storedMovies = localStorage.getItem('movies')
+let parsedMovies = JSON.parse(storedMovies)
+let favMoviesArr = []
+if (parsedMovies) {
+	noMoviesParagraph.innerHTML = ''
+	favMoviesArr = parsedMovies
+}
+
+const render = () => {
+	if (favMoviesArr) {
+		for (movie of favMoviesArr) {
+			favMovies.innerHTML += ` <div id =${movie.id} class="movie__card">
+		 <div class="movie__img">
+		  <img src=${movie.poster} alt="">
+		 </div>
+		 <div class="movie__info">
+		  <h3 class="movie__title">${movie.title}</h3>
+		  <div class="movie__details">
+		   <p class="time">${movie.time}</p>
+		   <p class="type">${movie.type}</p><button class="remove-btn"><i class="fa-solid fa-circle-minus"></i>
+			Delete movie</button>
+		  </div>
+		  <div class="movie__about">${movie.plot}</div>
+		 </div>
+		</div>`
+		}
+	}
+}
+
+document.addEventListener('click', e => {
+	const target = e.target.closest('.remove-btn')
+	if (target) {
+		const movieId = target.closest('.movie__card').id
+		favMoviesArr = favMoviesArr.filter(movie => movie.id !== movieId)
+		localStorage.setItem('movies', JSON.stringify(favMoviesArr))
+		favMovies.innerHTML = ''
+		favMoviesArr.forEach(movie => {
+			favMovies.innerHTML += ` <div id =${movie.id} class="movie__card">
+			<div class="movie__img">
+			 <img src=${movie.poster} alt="">
+			</div>
+			<div class="movie__info">
+			 <h3 class="movie__title">${movie.title}</h3>
+			 <div class="movie__details">
+			  <p class="time">${movie.time}</p>
+			  <p class="type">${movie.type}</p><button class="remove-btn"><i class="fa-solid fa-circle-minus"></i>
+			   Delete movie</button>
+			 </div>
+			 <div class="movie__about">${movie.plot}</div>
+			</div>
+		   </div>`
+		})
+	}
+})
+
+render()
---
+++ main.js
@@ -0,0 +1,61 @@
+const searchInput = document.querySelector('.search-input')
+const searchBtn = document.querySelector('.search-btn')
+const moviesBox = document.querySelector('.movies')
+const favMovies = document.querySelector('.movies-fav')
+const watchList = document.querySelector('.watchlist')
+
+const API_KEY = '7b891d'
+
+const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}&t=`
+const storedMovies = localStorage.getItem('movies')
+const parsedMovies = JSON.parse(storedMovies)
+let favMoviesArr = []
+if (parsedMovies) {
+	favMoviesArr = parsedMovies
+}
+
+function addMovie() {
+	localStorage.setItem('movies', JSON.stringify(favMoviesArr))
+}
+
+async function fetchMovies(e) {
+	const inputVal = searchInput.value
+
+	const response = await fetch(API_URL + inputVal)
+	if (response) {
+		const movie = await response.json()
+
+		favMoviesArr.unshift({
+			title: movie.Title,
+			poster: movie.Poster,
+			time: movie.Runtime,
+			type: movie.Genre,
+			plot: movie.Plot,
+			id: movie.imdbID,
+		})
+		console.log(favMoviesArr)
+
+		moviesBox.innerHTML = ` <div class="movie__card">
+ <div class="movie__img">
+  <img src=${movie.Poster} alt="">
+ </div>
+ <div class="movie__info">
+  <h3 class="movie__title">${movie.Title}</h3>
+  <div class="movie__details">
+   <p class="time">${movie.Runtime}</p>
+   <p class="type">${movie.Genre}</p><button class="add-btn"><i class="fa-solid fa-circle-plus"></i>Add to
+    watchlist</button>
+  </div>
+  <div class="movie__about">${movie.Plot}</div>
+ </div>
+</div>`
+	}
+}
+
+searchBtn.addEventListener('click', fetchMovies)
+document.addEventListener('click', e => {
+	const target = e.target.closest('.add-btn')
+	if (target) {
+		target.addEventListener('click', addMovie)
+	}
+})
